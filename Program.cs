6090203ce4de6849216d5ab160f1a6cffc4e526d
Miguel Mendes <mendesmiguel2007@gmail.com>
2026-03-27 22:45:09 -0300

    primeiro exercicio string, grito

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Digite o Que quiser e se tornara um grito! ");
+
+String textoDoUsuario = Console.ReadLine()!;
+
+String MensagemDeTexto = $" {textoDoUsuario}!".ToUpper();
+
+Console.WriteLine(MensagemDeTexto);
